20a2ee6363956155541047e8ee723888c04d8abc
pc <pc@yeannie-gim>
2022-01-04 15:56:35 +0900

    header 수정 적용

--- spring_4team/src/main/java/kr/or/test/spring_test/Controller/SettlementController.java
+++ spring_4team/src/main/java/kr/or/test/spring_test/Controller/SettlementController.java
@@ -1,9 +1,6 @@
 package kr.or.test.spring_test.Controller;
 
-import java.util.List;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
