1842e3cae05ef4db88c0cfa806e4840d304ec8c4
kangGayoung <ksmart4@ksmart.or.kr>
2022-01-07 16:44:55 +0900

    [강가영] 주문관리 페이징

--- spring_4team/src/main/java/kr/or/test/spring_test/service/OrderService.java
+++ spring_4team/src/main/java/kr/or/test/spring_test/service/OrderService.java
@@ -1,5 +1,6 @@
 package kr.or.test.spring_test.service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -33,8 +34,50 @@ public class OrderService {
 		return orderList;
 		
 	}
-	/*
-	 * public List<Map<String, Object>> getorderList(){ return
-	 * orderMapper.getorderList(null); }
-	 */
+	
+	public Map<String, Object> getOrderList(int currentPage){
+		
+		// 보여줄 행의 갯수
+		int rowPerPage = 5;
+		
+		// 로그인 이력 테이블 행의 갯수
+		double rowCount = orderMapper.getOrderCount(); //double 소수점형태로 담아짐
+	
+		// 마지막 페이지
+		int lastPage = (int) Math.ceil((rowCount / rowPerPage));	
+		
+		// 페이지 알고리즘
+		int startNum = 0;
+		startNum = (currentPage - 1) * rowPerPage;
+		
+		// 보여줄 시작 페이지 번호
+		int startPageNum = 1;
+		// 보여줄 마지막 페이지 번호
+		int endPageNum = 4;
+		
+		// 동적 페이지 구성( 7페이지 부터 )
+		if(currentPage > 3) {
+			startPageNum	= currentPage - 1;
+			endPageNum		= currentPage + 2;
+			if(endPageNum >= lastPage){
+				startPageNum = lastPage - 3;
+				endPageNum 	 = lastPage;
+			}
+		}
+		
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("startNum", startNum);
+		paramMap.put("rowPerPage", rowPerPage);
+		
+		List<Map<String, Object>> orderList =  orderMapper.getOrderList(paramMap);
+		
+		paramMap.clear();
+		
+		paramMap.put("lastPage", lastPage);
+		paramMap.put("orderList", orderList);
+		paramMap.put("startPageNum", startPageNum);
+		paramMap.put("endPageNum", endPageNum);
+		
+		return paramMap;
+	}
 }
