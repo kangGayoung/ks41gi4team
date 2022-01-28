21ad5632649d73de707e5c47021288ab7f5e4539
cjk <zhdltlxpfn1@gamil.com>
2022-01-28 09:44:53 +0900

    ㅇㅇ

--- spring_4team/src/main/java/kr/or/test/spring_test/Controller/MainController.java
+++ spring_4team/src/main/java/kr/or/test/spring_test/Controller/MainController.java
@@ -4,20 +4,10 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import kr.or.test.spring_test.dto.StaffInfo;
-import kr.or.test.spring_test.service.MainService;
-
 
 @Controller
 public class MainController {
 	
-	MainService mainService;
-	
-	public MainController(MainService mainService) {
-		this.mainService = mainService;
-	}
 	
 
 	@GetMapping("/")
