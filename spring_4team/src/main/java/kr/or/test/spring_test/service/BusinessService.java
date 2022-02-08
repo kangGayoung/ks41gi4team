6091a2a438a3af16e8f075e90d09681f9f927841
cjk <zhdltlxpfn1@gamil.com>
2022-02-08 09:54:03 +0900

    [일단]뭐했더라

--- spring_4team/src/main/java/kr/or/test/spring_test/service/BusinessService.java
+++ spring_4team/src/main/java/kr/or/test/spring_test/service/BusinessService.java
@@ -42,59 +42,13 @@ public class BusinessService {
 		return businessMapper.BusinessInfoList(null);
 	}
 	
-	//기본형 조회
-	public Map<String, Object> BusinessListPrint(int currentPage) {
+	//businessCode 검색
+	public Map<String,Object> BusinessSearchCode(String businessCode){
 		
-		int rowPerPage = 5;
-		
-		double rowCount = businessMapper.getBusinessListCount();
-		
-		int lastPage = (int) Math.ceil((rowCount/rowPerPage));
-		
-		int startNum = (currentPage - 1) * rowPerPage;
-		
-		int startPageNum = 1;
-		
-		int endPageNum = 4;
-		
-		if(currentPage > 3) {
-			startPageNum = currentPage - 1;
-			endPageNum = currentPage + 2;
-			if(endPageNum >= lastPage) {
-				startPageNum = lastPage - 3;
-				endPageNum = lastPage;
-			}
-		}
-		
-		
-		/* 예제로 작동하는거 확인 후 주석
-		if(currentPage > 6) {
-			startPageNum = currentPage - 5;
-			endPageNum = currentPage + 4;
-			if(endPageNum >= lastPage) {
-				startPageNum = lastPage - 9;
-				endPageNum = lastPage;
-			}
-		}
-		*/
-		Map<String,Object> paramMap = new HashMap<String, Object>();
-		
-		paramMap.put("startNum", startNum);
-		paramMap.put("rowPerPage", rowPerPage);
-		
-		List<Map<String,Object>> businessList = businessMapper.BusinessInfoList(paramMap);
-		
-		paramMap.clear();
-		
-		paramMap.put("lastPage", lastPage);
-		paramMap.put("businessList",businessList);
-		paramMap.put("startPageNum",startPageNum);
-		paramMap.put("endPageNum",endPageNum);
-		
-		return paramMap;
-		
+		return businessMapper.businessSearchCode(businessCode);
 	}
 	
+
 	
 	
 	
